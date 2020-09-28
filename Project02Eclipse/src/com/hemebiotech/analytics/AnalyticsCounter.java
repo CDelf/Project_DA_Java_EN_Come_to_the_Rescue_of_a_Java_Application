d95d1799138a046a6afea96af403ee18676203d2
cdelf <cdelf@DESKTOP-9519QGM>
2020-09-28 16:56:30 +0200

    Correction of Alex's code

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,41 +3,82 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+	/**
+	 * This is a proposition of correction of Alex's initial code.
+	 * 
+	 * It works whatever the number of symptoms contained by the file symptoms.txt
+	 * and allows the addition of new ones.
+	 * 
+	 * Exceptions handling is managed.
+	 * 
+	 * However this code doesn't respect the POO method.
+	 */
 
-			line = reader.readLine();	// get another symptom
+	public static String filepath = "symptoms.txt"; // This is the file containing the list of Symptoms
+
+	public static void main(String[] args) throws Exception {
+
+		/**
+		 * Arraylist of strings containing the list of symptoms. If no data is
+		 * available, return an empty List.
+		 * 
+		 * @return a raw listing of all Symptoms obtained from a data source, duplicates
+		 *         are possible/probable.
+		 */
+
+		ArrayList<String> getSymptoms = new ArrayList<>();
+
+		if (filepath != null) {
+			try {
+				BufferedReader readSymptoms = new BufferedReader(new FileReader(filepath));
+
+				String line = readSymptoms.readLine();
+
+				while (line != null) {
+					getSymptoms.add(line);
+					line = readSymptoms.readLine();
+
+				}
+				readSymptoms.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
+
+		/**
+		 * Creation of a map associating a string (=symptom) to a value (=number of
+		 * occurence in the list).
+		 * 
+		 * Utilisation of a TreeMap that automatically respects alphabetical order for
+		 * strings.
+		 */
+
+		Map<String, Integer> mapCounter = new TreeMap<String, Integer>();
+
+		for (String symptoms : getSymptoms) {
+
+			mapCounter.put(symptoms, mapCounter.get(symptoms) == null ? 1 : mapCounter.get(symptoms) + 1);
+		}
+
+		/**
+		 * Export the list of symptoms in natural ordrer with their occurences into a
+		 * .txt file "results.out"
+		 */
+
+		// Export dans un fichier txt results.out
+		FileWriter writer = new FileWriter("results.out.txt");
+		for (Map.Entry<String, Integer> entry : mapCounter.entrySet()) {
+			writer.write(entry.getKey() + " = " + entry.getValue());
+			writer.write(System.lineSeparator());
+		}
 		writer.close();
 	}
+
 }
