8447b21a20557b0b79e7af62964f8db5309e63d9
cdelf <cdelf@DESKTOP-9519QGM>
2020-09-28 17:00:28 +0200

    New class : WriteResultsFromMap.java

---
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteResultsFromMap.java
@@ -0,0 +1,27 @@
+package com.hemebiotech.analytics;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class WriteResultsFromMap {
+
+	/**
+	 * Exports the list of symptoms in natural ordrer with their occurences into a
+	 * .txt file "results.out"
+	 * 
+	 * @throws IOException
+	 */
+
+	public static void writeResult(Map<String, Integer> mapCounter, List<String> symptoms) throws IOException {
+
+		FileWriter writer = new FileWriter("results.out.txt");
+		for (Map.Entry<String, Integer> entry : mapCounter.entrySet()) {
+			writer.write(entry.getKey() + " = " + entry.getValue());
+			writer.write(System.lineSeparator());
+		}
+		writer.close();
+
+	}
+}
